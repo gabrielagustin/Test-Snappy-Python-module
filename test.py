9769077c14ce9f064433cc3e600f985f5007475d
gabrielagustin <gabiagus@gmail.com>
2019-09-04 08:56:05 -0300

    improving code

--- test.py
+++ test.py
@@ -9,7 +9,8 @@ from snappy import GPF
 GPF.getDefaultInstance().getOperatorSpiRegistry().loadOperatorSpis()
 HashMap = snappy.jpy.get_type('java.util.HashMap')
 
-Now loop through all Sentinel-1 data sub folders that are located within a super folder (of course, make sure, that the data is already unzipped):
+### Now loop through all Sentinel-1 data sub folders that are located within a super folder 
+### (of course, make sure, that the data is already unzipped):
 
 path = "D:\\SENTINEL\\"
  for folder in os.listdir(path):
@@ -20,12 +21,14 @@ path = "D:\\SENTINEL\\"
    timestamp = folder.split("_")[4] 
    date = timestamp[:8]
 
-Then, read in the Sentinel-1 data product:
+### Then, read in the Sentinel-1 data product:
 
    sentinel_1 = ProductIO.readProduct(output + "\\manifest.safe")    
    print sentinel_1
 
-If polarization bands are available, spolit up your code to process VH and VV intensity data separately. The first step is the calibration procedure by transforming the DN values to Sigma Naught respectively. You can specify the parameters to output the Image in Decibels as well.
+### If polarization bands are available, spolit up your code to process VH and VV intensity data separately. 
+### The first step is the calibration procedure by transforming the DN values to Sigma Naught respectively. 
+### You can specify the parameters to output the Image in Decibels as well.
 
    pols = ['VH','VV'] 
    for p in pols:  
@@ -43,7 +46,8 @@ If polarization bands are available, spolit up your code to process VH and VV in
       target_0 = GPF.createProduct("Calibration", parameters, sentinel_1) 
       ProductIO.writeProduct(target_0, calib, 'BEAM-DIMAP')
 
-Next, specify a subset AOI to reduce the data amount and processing time. The AOI specified by its outer polygon corners and is formatted through a Well Known Text (WKT).
+### Next, specify a subset AOI to reduce the data amount and processing time. The AOI specified by its outer 
+### polygon corners and is formatted through a Well Known Text (WKT).
 
       ### SUBSET
 
@@ -64,7 +68,9 @@ Next, specify a subset AOI to reduce the data amount and processing time. The AO
       ProductIO.writeProduct(target_1, subset, 'BEAM-DIMAP')
 
 
-Apply a Range Doppler Terrain Correction to correct for layover and foreshortening effects, by using the SRTM 3 arcsecond product (90m) that is downloaded automatically. You could also specify an own DEM product with a higher spatial resolution from a local path:
+### Apply a Range Doppler Terrain Correction to correct for layover and foreshortening effects,
+### by using the SRTM 3 arcsecond product (90m) that is downloaded automatically. You could also specify
+### an own DEM product with a higher spatial resolution from a local path:
 
       ### TERRAIN CORRECTION
  
